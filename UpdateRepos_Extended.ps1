cf62fc2370e791f3059eeafa1653a01000daaf42
Igor Grešovnik <gresovnik@gmail.com>
2025-08-24 21:29:21 +0200

    In UpdateRepos_Extended.ps1, added cloning/updating code documentation repositories

--- UpdateRepos_Extended.ps1
+++ UpdateRepos_Extended.ps1
@@ -12,17 +12,26 @@ Write-Host "Script directory: $scriptDir"
 # Write-Host "`n`nUpdating basic IGLib repositories:`n" # this output is already in script.
 & $(join-path $scriptDir "UpdateRepos_Basic.ps1")
 
-Write-Host "`nUpdating EXTENDED IGLib repositories:`n"
+Write-Host "`nUpdating EXTENDED IGLib repositories...`n"
 
 Write-Host "`nUpdating IGLibEventAggregator:"
 & $(Join-Path $scriptDir "_scripts/UpdateRepo_IGLibEventAggregator.ps1")
-
 
 # Write-Host "`nUpdating IGLibScriptsPS:"
 # & $(Join-Path $scriptDir "_scripts/UpdateRepo_IGLibScriptsPS.ps1")
 
 # Write-Host "`nUpdating IGLibScriptsEXP:"
 # & $(Join-Path $scriptDir "_scripts/UpdateRepo_IGLibScriptsEXP.ps1")
+
+
+Write-Host "`nUpdating Code Documentation repositories in _doc:"
+
+Write-Host "`nUpdating _doc/codedoc:"
+& $(Join-Path $scriptDir "_doc/UpdateRepo_codedoc.ps1")
+
+Write-Host "`nUpdating _doc/CodeDocumentation:"
+& $(Join-Path $scriptDir "_doc/UpdateRepo_CodeDocumentation.ps1")
+
 
 
 
