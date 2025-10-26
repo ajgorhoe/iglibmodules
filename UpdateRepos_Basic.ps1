86ee51bbd8f7483a5bd35a50e4748367eb57a681
Igor Grešovnik <gresovnik@gmail.com>
2025-10-26 09:17:51 +0100

    iglearn with embedded learning eepos in UpdateRepos_Basic.ps1 (at the end)

--- UpdateRepos_Basic.ps1
+++ UpdateRepos_Basic.ps1
@@ -11,10 +11,6 @@ Write-Host "`nUpdating BASIC IGLib repositories:`n"
 
 Write-Host "Script directory: $scriptDir"
 
-# Update learning repo as part of IGLibBasic:
-Write-Host "`nUpdating iglearn:"
-& $(Join-Path $scriptDir "_scripts/UpdateRepo_iglearn.ps1")
-
 Write-Host "`nUpdating IGLibCore:"
 & $(Join-Path $scriptDir "_scripts/UpdateRepo_IGLibCore.ps1")
 
@@ -35,6 +31,12 @@ Write-Host "`nUpdating IGLibSandbox:"
 
 # Remark:
 # Updating IGLibEventAggregator moved to UpdateRepos_Extended.ps1
+
+# Update learning repo as part of IGLibBasic:
+Write-Host "`nUpdating iglearn:"
+& $(Join-Path $scriptDir "_scripts/UpdateRepo_iglearn.ps1")
+Write-Host "`nUpdating iglearn's embedded repos:"
+& $(Join-Path $scriptDir "iglearn/UpdateRepoGroup_OtherRepos.ps1")
 
 Write-Host "  ... updating basic repositoris in iglibmodules/ completed.`n`n"
 
